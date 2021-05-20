dd1fdad26fdedfd8887dc5ff362495fb68c2ffca
Johana Requena <jha.requena@gmail.com>
2021-05-20 16:38:25 -0300

    Inicializacion del repositorio con configuraciones iniciales

---
+++ src/main/java/com/bigp/apisearchproduct/ApiSearchProductApplication.java
@@ -0,0 +1,13 @@
+package com.bigp.apisearchproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSearchProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSearchProductApplication.class, args);
+	}
+
+}
